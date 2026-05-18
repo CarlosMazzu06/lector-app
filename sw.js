39d7adc168ca5c1fa79c2305f4fc1589a31938c5
CarlosMazzu06 <130777947+CarlosMazzu06@users.noreply.github.com>
2026-05-18 15:45:27 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-self.addEventListener("install", (e) => self.skipWaiting());
-self.addEventListener("activate", () => console.log("Santuario Literario activo"));
-self.addEventListener("fetch", (e) => {});
+self.addEventListener("install",(e)=>self.skipWaiting());
+self.addEventListener("activate",()=>console.log("Santuario Literario activo"));
+self.addEventListener("fetch",(e)=>{});
